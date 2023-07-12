b89ddba166851876afbf710db804fd516f50f23e
pelicanStd <pelican8118@gmail.com>
2023-07-12 14:41:30 +0900

    youtube 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Main from "@/Routes/Main/Main";
 
 import "@/Common/Styles/global.scss";
+import Youtube from "@/Components/YouTube/Youtube.tsx";
 
 function App() {
   return (
     <>
       <Main />
+      <Youtube />
     </>
   );
 }
