31ec5a8e68aede9795107f8edd257d545ba52b63
pelicanStd <pelican8118@gmail.com>
2023-07-12 14:54:43 +0900

    youtube 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Main from "@/Routes/Main/Main";
 
 import "@/Common/Styles/global.scss";
-import Youtube from "@/Components/YouTube/Youtube.tsx";
 
 function App() {
   return (
     <>
       <Main />
-      <Youtube />
     </>
   );
 }
--- src/Routes/Main/Main.tsx
+++ src/Routes/Main/Main.tsx
@@ -1,10 +1,12 @@
 import Header from '@/Components/Header/Header';
 import Footer from '@/Components/Footer/Footer';
+import Youtube from "@/Components/YouTube/Youtube.tsx";
 
 export default function Main () {
   return (
     <div>
       <Header />
+      <Youtube />
       <Footer />
     </div>
   )
